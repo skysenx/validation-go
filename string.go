576b8830eb225341c27d8e3bfdef601346c635c8
Norman Osaruyi <norman@skysenx.com>
2021-01-21 14:42:50 +0100

    some work done

--- string.go
+++ string.go
@@ -7,14 +7,18 @@ import (
 
 //StringRule ..
 type StringRule struct {
-	Name  string
-	Min   int
-	Max   int
-	Value string
+	Name      string
+	Min       int
+	Max       int
+	AllowNull bool
+	Value     string
 }
 
 //Validate ..
 func (v *StringRule) Validate() (bool, error) {
+	if v.AllowNull && len(v.Value) == 0 {
+		return true, nil
+	}
 	if len(v.Value) < v.Min {
 		return false, errors.New(v.Name + " Minimum Length is " + strconv.FormatInt(int64(v.Min), 10))
 	}
