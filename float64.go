ff2a3d5fdb8ed4c1e5389cacad4a25801331d5ed
Norman Osaruyi <norman@skysenx.com>
2021-01-21 13:35:15 +0100

    some work done

--- float64.go
+++ float64.go
@@ -15,18 +15,18 @@ type Float64Rule struct {
 
 //Validate ..
 func (v *Float64Rule) Validate() (bool, error) {
-	value, err := strconv.ParseFloat(v.Value, 10, 16)
+	value, err := strconv.ParseFloat(v.Value, 64)
 
 	if err != nil {
 		return false, errors.New(v.Name + " Invalid")
 	}
 
 	if value < v.Min {
-		return false, errors.New(v.Name + " Minimum is " + strconv.FormatFloat(v.Min, 16))
+		return false, errors.New(v.Name + " Minimum is " + strconv.FormatFloat(v.Min, 'f', -1, 64))
 	}
 
 	if value > v.Max {
-		return false, errors.New(v.Name + " Maximum is " + strconv.FormatFloat(v.Max, 16))
+		return false, errors.New(v.Name + " Maximum is " + strconv.FormatFloat(v.Max, 'f', -1, 64))
 	}
 
 	return true, nil
